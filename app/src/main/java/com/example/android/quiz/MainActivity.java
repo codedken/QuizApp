7ca089cce83292b33441a1c4b542124dcb5bedb4
codedken <cbsmoothoj@gmail.com>
2018-06-30 21:46:11 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/android/quiz/MainActivity.java
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        getSupportActionBar().setTitle("Tech Quiz");
     }
 
     public void checkRadioButtonQuestion1IsClicked(){
